ef24b2407d89da218659baff450d61f04004c462
Manoj Kumar <talk2manoj@gmail.com>
2016-11-30 11:43:36 +0530

    Disabled JSON REST APIs

--- theme/openinnovation-toolkit/functions.php
+++ theme/openinnovation-toolkit/functions.php
@@ -17,6 +17,17 @@ define( 'METHODS_PAGE_URL', SITE_URL.'/methods/' );
 define( 'SUBMIT_PAGE_URL', SITE_URL.'/contribute/' );
 
 
+function disable_json_api () {
+  // Filters for WP-API version 1.x
+  add_filter('json_enabled', '__return_false');
+  add_filter('json_jsonp_enabled', '__return_false');
+
+  // Filters for WP-API version 2.x
+  add_filter('rest_enabled', '__return_false');
+  add_filter('rest_jsonp_enabled', '__return_false');
+}
+add_action( 'after_setup_theme', 'disable_json_api' );
+
 /*
  * GLOBAL VARIABLES
  */
