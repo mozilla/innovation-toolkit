a503cd85c0b06999bd7cfa2227f8e6a7340698a6
Manoj Kumar <talk2manoj@gmail.com>
2016-12-06 16:01:39 +0530

    reCaptcha Testing

--- theme/openinnovation-toolkit/header.php
+++ theme/openinnovation-toolkit/header.php
@@ -11,7 +11,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
-  <?php if(is_page(214)) :
+  <?php if(is_page(1259)) :
     echo '<script src="https://www.google.com/recaptcha/api.js?onload=loadCaptcha&render=explicit" async defer></script>';
   endif; ?>
 </head>
