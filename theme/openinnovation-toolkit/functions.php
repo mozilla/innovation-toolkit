0b4e5fc8923e0a171daee4a9b342018ab710ccab
Manoj Kumar <talk2manoj@gmail.com>
2016-12-08 12:16:06 +0530

    testing recaptcha

--- theme/openinnovation-toolkit/functions.php
+++ theme/openinnovation-toolkit/functions.php
@@ -221,13 +221,14 @@ function twentysixteen_scripts() {
   wp_enqueue_style('font-mozilla-fira', 'https://code.cdn.mozilla.net/fonts/fira.css', array(), false, 'screen,projection,print');
   wp_enqueue_style('font-CooperHewitt', THEME_PATH.'/fonts/fonts.css', array(), false, 'screen,projection,print');
   
-  // Google Recaptcha.
-  wp_enqueue_style('recaptcha', 'https://www.google.com/recaptcha/api.js?render=explicit', array('jquery'), false, 'screen,projection');
-
 	// Theme stylesheet.
 	wp_enqueue_style( 'twentysixteen-style', get_stylesheet_uri() );
   wp_enqueue_style( 'twentysixteen-styles', THEME_PATH.'/stylesheets/styles.css' );
 
+  
+  // Google Recaptcha.
+  wp_enqueue_script('recaptcha', 'https://www.google.com/recaptcha/api.js?render=explicit', array('jquery'), '', false);
+  
   // Loading javascripts and jquery plugins
   wp_enqueue_script('jquery-easing', THEME_PATH . '/js/jquery.easing.1.3.js', array('jquery'), '1.3');
   wp_enqueue_script('jquery-showLoading', THEME_PATH . '/js/jquery.showLoading.min.js', array('jquery'), '1.0', false);
